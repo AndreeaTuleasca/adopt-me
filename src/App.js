f6a3ac0f304a3f7aed10fd9727e9d8b072b5ca2d
Andreea Tuleasca <andreea.tuleasca@gmail.com>
2019-10-29 18:53:02 +0200

    call server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
+import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-  ]);
+  return (
+    <React.StrictMode>
+      <div>
+        <h1>Adopt me!</h1>
+        <SearchParams></SearchParams>
+      </div>
+    </React.StrictMode>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
