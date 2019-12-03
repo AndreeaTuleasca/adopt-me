96188928475e0f1849922d73a740c995f4d474c0
Andreea <andreea@Bytexs-MacBook-Pro.local>
2019-12-03 13:31:19 +0200

    add animation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
-import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
 
+const spin = keyframes`
+to {
+    transform: rotate(360deg);
+}`;
 const NavBar = () => {
-    const [padding, setPadding] = useState(15);
-    return (
-        <header
-        onClick={() => setPadding(padding+15)}
-            css={css`
-                background-color: #DCAE96;
-                padding: ${padding}px;
-            `}>
-            <Link to="/">Adopt me!</Link>
-            <span>Log in</span>
-        </header>
-    )
-}
+  const [padding, setPadding] = useState(15);
+  return (
+    <header
+      css={css`
+        background-color: ${colors.primary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt me!</Link>
+      <span
+        role="img"
+        aria-label="logo"
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+          &:hover {
+            animation: 1s ${spin} linear infinite reverse;
+          }
+        `}
+      >
+        🌍
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
